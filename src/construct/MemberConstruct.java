669aa7d6d3d5128ba181bfb26f577564df0e0ff5
developerjjoa <developerjjoa@gmail.com>
2025-07-09 17:00:37 +0900

    생성자 오버로딩과 this()

--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -5,6 +5,15 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    //추가
+    public MemberConstruct(String name, int age) {
+        this(name, age, 50);
+        //중복되는 코드 생략 가능
+        //this.name = name;
+        //this.age = age;
+        //this.grade = 50;
+    }
+
     MemberConstruct(String name, int age, int grade) {
         System.out.println("생성자 호출 name=" + name + ", age=" + age + ", grade=" + grade);
         this.name = name;
