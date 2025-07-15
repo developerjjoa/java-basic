ef5ad136be02cd3cf96403389510b1b032a4ac9b
developerjjoa <developerjjoa@gmail.com>
2025-07-15 16:16:22 +0900

    static 메서드 3 - static import

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,9 +1,13 @@
 package static2;
 
+import static static2.DecoData.*;
+//import static static2.DecoData.staticCall;
+
 public class DecoDataMain {
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
-        DecoData.staticCall();
+        //DecoData.staticCall();
+        staticCall();
 
         System.out.println("2. 인스턴스 호출1");
         DecoData data1 = new DecoData();
@@ -14,6 +18,6 @@ public class DecoDataMain {
         data2.instanceCall();
 
         System.out.println("4. 정적 메서드에 참조값을 매개변수로 받아 인스턴스 변수와 메서드 사용하기");
-        DecoData.staticCall(data1);
+        staticCall(data1);
     }
 }
