b6597007907344160c4edaff9d7f26715ffd9a2d
developerjjoa <developerjjoa@gmail.com>
2025-07-16 17:45:41 +0900

    final 필드(멤버 변수) final을 필드에 사용할 경우 해당 필드는 생성자를 통해서 한번만 초기화 될 수 있다.

---
+++ src/final1/ConstructInit.java
@@ -0,0 +1,11 @@
+package final1;
+
+public class ConstructInit {
+    final int value;
+
+    //생성자를 통해서 값을 넣어줘야 함
+    public ConstructInit(int value) {
+        this.value = value;
+    //이후에는 값을 바꾸는게 불가능
+    }
+}
